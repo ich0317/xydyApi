f8c6973e87a279e2df56132efd3fb4396e712cc6
cl <ich0317@yeah.net>
2019-07-04 23:25:43 +0800

    74

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose"); //数据库
 let jwt = require('jsonwebtoken');
 let routerApi = require("./routes/api");
 
+
    
 app.use(
   bodyParser.urlencoded({
@@ -21,20 +22,26 @@ app.use(
 
 
 app.use('/uploads', express.static(__dirname + "/uploads")); //文件托管
-
+console.dir(routerApi.stack[0].route.path);
 //登录拦截
 app.all('/*', function(req, res, next){
-
+console.dir(req);
   let getToken = req.headers['x-token'];
+  let getClientRouter = req.url;  //获取客户端访问路由
 
-  if(req.headers.referer == 'http://192.168.0.107:8082/'){
+  if(req.headers.referer.indexOf('http://192.168.0.107:8082/')!=-1){
+   
     //前台
     if(!!getToken){
+      
+      //有token
       jwt.verify(getToken, 'b1234', function(err, decoded) {
-   
+       
         if(decoded){
+        
           next();
         }else{
+          
           res.json({
             code:20003,
             msg:'登录已过期'
@@ -42,12 +49,17 @@ app.all('/*', function(req, res, next){
         }
       });
     }else{
-      res.json({
-        code:20003,
-        msg:'登录已过期'
-      });
+      if(getClientRouter.indexOf('/api/userLogin') != -1){
+        next()
+      }else{
+        res.json({
+          code:20003,
+          msg:'请先登录'
+        });
+      }
     }
   }else{
+   
     //后台
     if(!!getToken){
       jwt.verify(getToken, 'a1234', function(err, decoded) {
@@ -67,7 +79,7 @@ app.all('/*', function(req, res, next){
   }
 })
 
-app.use('/', routerApi);
+app.use('/api', routerApi);
  
 mongoose.connect("mongodb://localhost:27017/xydy", {
   useNewUrlParser: true
--- routes/api.js
+++ routes/api.js
@@ -18,94 +18,94 @@ const newsManage = require('../controller/back/news');
  */
 
 //获取城市
-router.get("/api/getCityList", film.getCityList);
+router.get("/getCityList", film.getCityList);
 //获取影院列表
-router.get("/api/getCinemaList", film.getCinemaList); 
+router.get("/getCinemaList", film.getCinemaList); 
 //获取排期
-router.get("/api/getIndexFilmList", film.getIndexFilmList); 
+router.get("/getIndexFilmList", film.getIndexFilmList); 
 //获取定位学校
-router.get("/api/getLocationCollege", film.getLocationCollege);
+router.get("/getLocationCollege", film.getLocationCollege);
 //获取座位图
-router.get("/api/getSeat", film.getSeat);
+router.get("/getSeat", film.getSeat);
 //提交订单
-router.post("/api/placeOrder", order.placeOrder);
+router.post("/placeOrder", order.placeOrder);
 //获取新闻列表
-router.get("/api/getFindNew", news.getFindNew);
+router.get("/getFindNew", news.getFindNew);
 //获取新闻详情
-router.get("/api/getFindDetail", news.getFindDetail);
+router.get("/getFindDetail", news.getFindDetail);
 //新闻点赞
-router.post("/api/like", news.like);
+router.post("/like", news.like);
 //登录
-router.post("/api/userLogin", user.userLogin);
+router.post("/userLogin", user.userLogin);
 
 /**
  **** 后台api
  */
 
 //登录
-router.post("/api/login", loginManage.login);
+router.post("/login", loginManage.login);
 //添加学校
-router.post("/api/addCollege", cinemaManage.addCollege);
+router.post("/addCollege", cinemaManage.addCollege);
 //搜索学校
-router.post("/api/searchCollege", cinemaManage.searchCollege);
+router.post("/searchCollege", cinemaManage.searchCollege);
 //获取学校
-router.get("/api/getCollege", cinemaManage.getCollege);
+router.get("/getCollege", cinemaManage.getCollege);
 //删除学校
-router.post("/api/delCollege", cinemaManage.delCollege);
+router.post("/delCollege", cinemaManage.delCollege);
 /**
  * 影院管理
  */
 //添加影院
-router.post("/api/addCinema", cinemaManage.addCinema);
+router.post("/addCinema", cinemaManage.addCinema);
 //获取影院列表
-router.get("/api/getCinema", cinemaManage.getCinema);
+router.get("/getCinema", cinemaManage.getCinema);
 //获取影院详情
-router.get("/api/getCinemaDetail", cinemaManage.getCinemaDetail);
+router.get("/getCinemaDetail", cinemaManage.getCinemaDetail);
 //删除影院
-router.post("/api/delCinema", cinemaManage.delCinema);
+router.post("/delCinema", cinemaManage.delCinema);
 //添加影片
-router.post("/api/addFilm", filmManage.addFilm);
+router.post("/addFilm", filmManage.addFilm);
 //添加影片海报
-router.post("/api/upFilmPhoto", filmManage.upFilmPhoto);
+router.post("/upFilmPhoto", filmManage.upFilmPhoto);
 //获取影片列表
-router.post("/api/getFilmList", filmManage.getFilmList);
+router.post("/getFilmList", filmManage.getFilmList);
 //获取影片详情
-router.get("/api/getFilmDetail", filmManage.getFilmDetail);
+router.get("/getFilmDetail", filmManage.getFilmDetail);
 //删除影片
-router.post("/api/delFilm", filmManage.delFilm);
+router.post("/delFilm", filmManage.delFilm);
 //排期 搜索影片
-router.post("/api/searchFilm", planManage.searchFilm);
+router.post("/searchFilm", planManage.searchFilm);
 //添加影厅
-router.post("/api/addScreen", screenManage.addScreen);
+router.post("/addScreen", screenManage.addScreen);
 //获取影厅
-router.get("/api/getScreen", screenManage.getScreen);
+router.get("/getScreen", screenManage.getScreen);
 //添加座位
-router.post("/api/addSeat", screenManage.addSeat);
+router.post("/addSeat", screenManage.addSeat);
 //获取座位
-router.post("/api/getSeat", screenManage.getSeat);
+router.post("/getSeat", screenManage.getSeat);
 /**
  * 添加排期栏目
  */
 //添加排期
-router.post("/api/addSession", planManage.addSession);
+router.post("/addSession", planManage.addSession);
 //获取影厅和排期
-router.get("/api/getScreenSession", planManage.getScreenSession);
+router.get("/getScreenSession", planManage.getScreenSession);
 //删除排期
-router.post("/api/delSession", planManage.delSession);
+router.post("/delSession", planManage.delSession);
 //审核排期
-router.post("/api/agreeSession", planManage.agreeSession);
+router.post("/agreeSession", planManage.agreeSession);
 /**
  * 新闻栏目
  */
 //添加新闻图片
-router.post("/api/upNewsPhoto", newsManage.upNewsPhoto);
+router.post("/upNewsPhoto", newsManage.upNewsPhoto);
 //添加新闻
-router.post("/api/addNews", newsManage.addNews);
+router.post("/addNews", newsManage.addNews);
 //获取新闻列表
-router.get("/api/getNewsList", newsManage.getNewsList);
+router.get("/getNewsList", newsManage.getNewsList);
 //获取新闻详情
-router.get("/api/getNewsDetail", newsManage.getNewsDetail);
+router.get("/getNewsDetail", newsManage.getNewsDetail);
 //删除新闻
-router.post("/api/delNews", newsManage.delNews);
+router.post("/delNews", newsManage.delNews);
 
 module.exports = router;
