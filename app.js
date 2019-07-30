d29487b3e8979a71210a8d2c446dc1903a2004ea
changliang <changliang@campuscinema.cn>
2019-07-30 11:59:17 +0800

    test

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 //   cookie: { maxAge :1000000 }
 // }))
 
-express.use(require('express').static(path.join(__dirname,"/uploads"))) //文件托管
+app.use('/uploads', express.static(__dirname + "/uploads")); //文件托管
 
 //登录拦截
 app.all('/*', function(req, res, next){
