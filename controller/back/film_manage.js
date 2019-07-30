45f192cece92926512f9f1be487225c07e2322fb
changliang <changliang@campuscinema.cn>
2019-07-30 13:59:29 +0800

    test

--- controller/back/film_manage.js
+++ controller/back/film_manage.js
@@ -74,7 +74,7 @@ exports.upFilmPhoto = (req, res, next) => {
       code: 0,
       msg: "上传成功",
       data: {
-        imgUrl: `${process.env.baseUrl}${upPath}`
+        imgUrl: `https://localhost:8084/${upPath}`
       }
     });
   });
